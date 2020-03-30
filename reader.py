37bbf75da4856e2e330d7ff0ce6e7745cd2dff8f
Keigh Rim <keigh.rim@gmail.com>
2020-03-30 15:50:55 -0400

    file reader can now read first N files only from each labeled dir

--- reader.py
+++ reader.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 
-def read_wavs(data_dir, file_ext='.wav'):
+def read_wavs(data_dir, file_ext='.wav', file_per_dir=sys.maxsize):
     for r, ds, fs in os.walk(data_dir):
         if len(ds) == 0:
-            for f in [f for f in fs if f.endswith(file_ext)]:
+            for f in [f for f in fs if f.endswith(file_ext)][:file_per_dir]:
                 yield r, f
 
 
