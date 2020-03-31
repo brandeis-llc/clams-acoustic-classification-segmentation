286eba2ca8b2609ecf7ca7926a424a10de3f20dd
Keigh Rim <keigh.rim@gmail.com>
2020-03-30 22:20:10 -0400

    bugfix in smoothing methods

--- smoothing.py
+++ smoothing.py
@@ -4,7 +4,8 @@ import numpy as np
 def minimum_change_support(predictions: np.ndarray, minimum_window_size=300):
     for i in range(1, len(predictions)):
         cur_label = predictions[i]
-        if cur_label != 0 and np.sum(predictions[max(0, i - minimum_window_size)] == cur_label) < (minimum_window_size // 2):
+        minimum_window = predictions[max(0,i - minimum_window_size):i]
+        if cur_label != 0 and np.sum(minimum_window == cur_label) < (len(minimum_window) // 2):
             predictions[i] = predictions[i-1]
 
 
@@ -12,8 +13,7 @@ def mode_smooth(predictions: np.ndarray, smooth_window=20):
     from scipy import stats
     for i in range(len(predictions)):
         s = max(0, i-smooth_window)
-        e = min(len(predictions), i+smooth_window)
-        # print(predictions[s:e])
+        e = min(len(predictions), i+1+smooth_window)
         predictions[i] = stats.mode(predictions[s:e])[0]
 
 
@@ -36,7 +36,7 @@ def frames_to_durations(predictions):
         segments[cur_speech_segment_started] = len(predictions)-1
 
     for start, end in segments.items():
-        print(f'{start / 100}\t{end/100}', end='\t')
+        print(f'{start/100}\t{end/100}', end='\t')
     print('\n')
 
 
