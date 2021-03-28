6cf819906d828ef261e495c4329e60b42e64106d
Keigh Rim <keigh.rim@gmail.com>
2021-03-28 19:30:55 -0400

    bugfix in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+with open('requirements.txt') as requirements:
+    requires = requirements.readlines()
+
 setup(
     name='brandeis-acs',
-    version='1.3',
+    version='1.4',
     packages=['bacs'],
     url='https://github.com/brandeis-llc/acoustic-classification-segmentation',
     license='MIT',
     author='Keigh Rim',
     author_email='krim@brandeis.edu',
     description='Brandeis Acoustic Classification & Segmentation tool',
+    install_requires=requires,
     entry_points={
         'console_scripts': [
             'bacs = bacs.run:main'
