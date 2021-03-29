24b876738ab71d46335c86cf1c70be4f8dd4854d
Keigh Rim <keigh.rim@gmail.com>
2021-03-28 21:48:09 -0400

    default mode path is now retrievable in the package

--- bacs/__init__.py
+++ bacs/__init__.py
@@ -1,2 +1,6 @@
 import os
 __all__ = [py[:-3] for py in os.listdir(os.path.dirname(__file__)) if py.endswith('.py') and py != '__init__.py']
+
+from . import *
+
+defmodel_path = os.path.join(os.path.dirname(__file__), 'defmodel')
