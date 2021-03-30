19041b5d453ca6136687965b7111bfa9a5f68aa6
Keigh Rim <keigh.rim@gmail.com>
2021-03-29 20:30:14 -0400

    bugfix in getting audio files
    bugfix in getting audio files

--- bacs/reader.py
+++ bacs/reader.py
@@ -11,10 +11,10 @@ def read_sph(r, f):
         return os.path.dirname(tempf.name), os.path.basename(tempf.name)
 
 
-def read_audios(data_dir, file_ext=['wav', 'mp3', 'sph'], file_per_dir=sys.maxsize):
+def read_audios(data_dir, file_ext=('wav', 'mp3', 'sph'), file_per_dir=sys.maxsize):
     for r, ds, fs in os.walk(data_dir):
         for f in fs[:file_per_dir]:
-            if os.path.splitext(f)[-1] in file_ext:
+            if f.split('.')[-1] in file_ext:
                 if f.endswith('.sph'):
                     yield read_sph(r, f)
                 else:
